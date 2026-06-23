a568832df5a1b5f6b4dac3e69bea16c2062378d8
Kavana-DR <kavanadr45@gmail.com>
2026-06-23 11:42:03 +0530

    `Updated file paths in app.ts to use process.cwd() instead of __dirname`

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,25 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
 
 import { errorHandler } from "./middleware/error-handler";
 import { notFound } from "./middleware/not-found";
 import { router } from "./routes";
 
-
 export const app = express();
 
 app.disable("x-powered-by");
 app.use(express.json({ limit: "1mb" }));
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(
-  express.static(path.join(__dirname, "../frontend-dist"))
+  express.static(path.join(process.cwd(), "frontend-dist"))
 );
 
 app.get("/", (_req, res) => {
   res.sendFile(
-    path.join(__dirname, "../frontend-dist/index.html")
+    path.join(process.cwd(), "frontend-dist", "index.html")
   );
 });
+
 app.use(router);
 app.use(notFound);
 app.use(errorHandler);
