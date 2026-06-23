8b64880795e69076e5baf1889a0019c09964c7ab
Kavana-DR <kavanadr45@gmail.com>
2026-06-23 11:47:11 +0530

    Remove frontend serving

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 
 import { errorHandler } from "./middleware/error-handler";
 import { notFound } from "./middleware/not-found";
@@ -9,17 +8,6 @@ export const app = express();
 
 app.disable("x-powered-by");
 app.use(express.json({ limit: "1mb" }));
-
-app.use(
-  express.static(path.join(process.cwd(), "frontend-dist"))
-);
-
-app.get("/", (_req, res) => {
-  res.sendFile(
-    path.join(process.cwd(), "frontend-dist", "index.html")
-  );
-});
-
 app.use(router);
 app.use(notFound);
 app.use(errorHandler);
